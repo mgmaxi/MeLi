f3f7f81a5b8b7ff79518dbabd131531a468b9c0e
mgmaxi <maximiliano.ghirardi@gmail.com>
2022-06-09 23:40:39 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import Home from './screens/Home/Home.js';
+import Products from './screens/Products/Products';
+import ProductDetail from './screens/ProductDetail/ProductDetail';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="items" element={<Products />} />
+          <Route path="items/:id" element={<ProductDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
