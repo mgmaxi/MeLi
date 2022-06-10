d8263a2fb16321243941ef3ffc195bf3dcde021f
mgmaxi <maximiliano.ghirardi@gmail.com>
2022-06-09 23:15:38 -0300

    Add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import './header.css';
+import FullLogo from '../../assets/images/full-logo.png';
+import Logo from '../../assets/images/logo.png';
+import Freesend from '../../assets/images/free-send.png';
+import { IoIosSearch } from 'react-icons/io';
+import { FiMapPin } from 'react-icons/fi';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { BsCart2 } from 'react-icons/bs';
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <div className="header-main">
+          <img src={FullLogo} alt="" className="full-logo" />
+          <img src={Logo} alt="" className="logo" />
+          <form action="" className="header-main-form">
+            <input
+              type="search"
+              className="search"
+              placeholder="Buscar productos, marcas y más.."
+            />
+            <button type="submit" className="search-btn">
+              <IoIosSearch />
+            </button>
+          </form>
+          <img
+            className="header-main-free-send"
+            src={Freesend}
+            alt="Free send"
+          />
+          <ul className="header-main-responsive-menu">
+            <li>
+              <AiOutlineMenu />
+            </li>
+            <li>
+              <a href="/">
+                <BsCart2 />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="header-menu">
+          <button className="header-menu-send">
+            <FiMapPin className="map-icon" />
+            <div>
+              <span className="header-menu-send-to">Enviar a</span>
+              <span className="header-menu-send-location">Capital Federal</span>
+            </div>
+          </button>
+          <nav className="header-menu-navbar">
+            <ul>
+              <li>
+                <a href="/">Categorías</a>
+              </li>
+              <li>
+                <a href="/">Ofertas</a>
+              </li>
+              <li>
+                <a href="/">Historial</a>
+              </li>
+              <li>
+                <a href="/">Supermercado</a>
+              </li>
+              <li>
+                <a href="/">Moda</a>
+              </li>
+              <li>
+                <a href="/">Vender</a>
+              </li>
+              <li>
+                <a href="/">Ayuda</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="header-menu-profile">
+            <ul>
+              <li>
+                <a href="/">Creá tu cuenta</a>
+              </li>
+              <li>
+                <a href="/">Ingresá</a>
+              </li>
+              <li>
+                <a href="/">Mis compras</a>
+              </li>
+              <li className="cart-icon">
+                <a href="/">
+                  <BsCart2 />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
